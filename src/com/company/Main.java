933fab453e22b436290210fc7d6908dae27e1596
chainley56 <31398635+chainley56@users.noreply.github.com>
2017-08-28 04:25:12 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+var one = 0;
+var two = 1;
+var three;
+
+var num = prompt(10,0);
+
+document.write(var1+"<br />");
+document.write(var2+"<br />");
+
+for(var i=3; i <= num;i++)
+{
+  var3 = var1 + var2;
+  var1 = var2;
+  var2 = var3;
+
+  document.write(var3+"<br />");
+}   
+
     }
 }
